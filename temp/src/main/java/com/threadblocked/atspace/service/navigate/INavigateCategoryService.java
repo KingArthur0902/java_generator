64d2dd1aff2f0282d86408483155957e5923d813
Arthur Tsang <kingarthur0902@gmail.com>
2016-02-15 22:36:56 +0800

    delete src file in temp

--- temp/src/main/java/com/threadblocked/atspace/service/navigate/INavigateCategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.threadblocked.atspace.service.navigate;
-
-import com.threadblocked.atspace.commons.base.IBaseEntityManager;
-import com.threadblocked.atspace.module.navigate.domain.NavigateCategory;
-
-public interface INavigateCategoryService extends IBaseEntityManager<NavigateCategory, java.lang.Integer>{
-
-}
--- temp/src/main/java/com/threadblocked/atspace/service/navigate/INavigateItemService.java
+++
@@ -1,8 +0,0 @@
-package com.threadblocked.atspace.service.navigate;
-
-import com.threadblocked.atspace.commons.base.IBaseEntityManager;
-import com.threadblocked.atspace.module.navigate.domain.NavigateItem;
-
-public interface INavigateItemService extends IBaseEntityManager<NavigateItem, java.lang.Integer>{
-
-}
