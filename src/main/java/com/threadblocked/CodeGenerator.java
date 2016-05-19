9e043ebcbd1bf04809fff9585f75270968df5e01
Arthur Tsang <kingarthur0902@gmail.com>
2016-05-19 19:50:57 +0800

    格式化

--- src/main/java/com/threadblocked/CodeGenerator.java
+++ src/main/java/com/threadblocked/CodeGenerator.java
@@ -5,40 +5,37 @@ import cn.org.rapid_framework.generator.GeneratorProperties;
 
 /**
  * Hello world!
- *
  */
-public class CodeGenerator
-{
+public class CodeGenerator {
     /**
      * @param args 接收表名，按照表来生成对于的模型
-	 * 请直接修改以下代码调用不同的方法以执行相关生成任务.
-	 */
-	public static void main(String[] args) throws Exception {
-		GeneratorFacade g = new GeneratorFacade();
+     *             请直接修改以下代码调用不同的方法以执行相关生成任务.
+     */
+    public static void main(String[] args) throws Exception {
+        GeneratorFacade g = new GeneratorFacade();
         GeneratorProperties.setProperty("moduleName", "wapdetail_t");
-//		g.printAllTableNames();				//打印数据库中的表名称
+        //g.printAllTableNames();				//打印数据库中的表名称
 
-		g.deleteOutRootDir();							//删除生成器的输出目录
-//      String tables = "PRD_INF_ARTICLE,PRD_BUSINESS_RULE,PRD_PAY_WAY,PRD_PRESENT,PRD_DELIVERY_LOGISTICS_COMPANY,PRD_DELIVERY_WAY,PRD_DELIVERY_RULE,PRD_DELIVERY_RULE_ZONE_RELATIONSHIP";
-//      String tables = "SYS_PARAM_INF,SYS_COMMENT,SYS_COMMENT_SESSION";
-//      String tables = "PRD_INF_ARTICLE";
-//	    String tables = "PRD_DELIVERY_LOGISTICS_COMPANY,PRD_DELIVERY_WAY,PRD_DELIVERY_RULE,PRD_DELIVERY_RULE_ZONE_RELATIONSHIP";
+        g.deleteOutRootDir();                            //删除生成器的输出目录
+        //String tables = "PRD_INF_ARTICLE,PRD_BUSINESS_RULE,PRD_PAY_WAY,PRD_PRESENT,PRD_DELIVERY_LOGISTICS_COMPANY,PRD_DELIVERY_WAY,PRD_DELIVERY_RULE,PRD_DELIVERY_RULE_ZONE_RELATIONSHIP";
+        //String tables = "SYS_PARAM_INF,SYS_COMMENT,SYS_COMMENT_SESSION";
+        //String tables = "PRD_INF_ARTICLE";
+        //String tables = "PRD_DELIVERY_LOGISTICS_COMPANY,PRD_DELIVERY_WAY,PRD_DELIVERY_RULE,PRD_DELIVERY_RULE_ZONE_RELATIONSHIP";
 
-		//PRD_USER_PRODUCT_PROXY,PRD_PAGE_MODULE,PRD_PAGE_MODULE_OBJECT
-	    String tables = "wapdetail_item,wapdetail_item_content,wapdetail_generate_parameter";
+        String tables = "wapdetail_item,wapdetail_item_content,wapdetail_generate_parameter";
 
         //设置生成器的模板目录
         g.getGenerator().setTemplateRootDir("E:\\workspace\\idea\\java_generator\\template\\");
 
-        for(String table : tables.split(",")){
-           g.generateByTable(table);
+        for (String table : tables.split(",")) {
+            g.generateByTable(table);
         }
-//		g.generateByTable("table_name","template");	//通过数据库表生成文件,template为模板的根目录
-		//g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
-//		g.generateByClass(Blog.class,"template_clazz");
-		 
-//		g.deleteByTable("table_name", "template"); //删除生成的文件
-		//打开文件夹
-		//Runtime.getRuntime().exec("cmd.exe /c start "+GeneratorProperties.getRequiredProperty("outRoot"));
-	}
+        //.generateByTable("table_name","template");	//通过数据库表生成文件,template为模板的根目录
+        //g.generateByAllTable("template");	//自动搜索数据库中的所有表并生成文件,template为模板的根目录
+        //g.generateByClass(Blog.class,"template_clazz");
+
+        //g.deleteByTable("table_name", "template"); //删除生成的文件
+        //打开文件夹
+        //Runtime.getRuntime().exec("cmd.exe /c start "+GeneratorProperties.getRequiredProperty("outRoot"));
+    }
 }
