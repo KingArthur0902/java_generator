9a1e0fa3b57f33b8d19009d8713d080e1d7b65be
ArthurTsang <kingarthur0902@gmail.com>
2016-05-20 16:10:04 +0800

    repo添加方法

--- template/src/main/java/${basepackage_dir}/${moduleName_dir}/repository/${className}Repository.java
+++ template/src/main/java/${basepackage_dir}/${moduleName_dir}/repository/${className}Repository.java
@@ -7,5 +7,9 @@ import ${basepackage}.${moduleName}.domain.${className};
 import org.springframework.data.jpa.repository.Query;
 
 public interface  ${className}Repository extends BaseRepository<${className},Long>{
-
+<#list table.columns as column>
+<#if column.pk>
+    ${className} findBy${column.columnName}(${column.javaType} ${column.columnNameLower});
+</#if>
+</#list>
 }
